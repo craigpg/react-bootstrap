97f80f4f2c47204e8e5ba097dc25d1cc07363d66
Craig Morrissey <craig@shineboxconsulting.com>
2019-09-03 16:23:24 -0400

    Fix FormControl onChange typescript declaration

--- types/components/FormControl.d.ts
+++ types/components/FormControl.d.ts
@@ -2,23 +2,25 @@ import * as React from 'react';
 import Feedback from './Feedback';
 import { BsPrefixComponent } from './helpers';
 
-export interface FormControlProps {
+export interface FormControlProps<T> {
   innerRef?: React.LegacyRef<this>;
   size?: 'sm' | 'lg';
   plaintext?: boolean;
   readOnly?: boolean;
   disabled?: boolean;
   value?: string;
-  onChange?: React.FormEventHandler<this>;
+  onChange?: React.FormEventHandler<T>;
   type?: string;
   id?: string;
   isValid?: boolean;
   isInvalid?: boolean;
 }
 
+type FormControlElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
+
 declare class FormControl<
   As extends React.ElementType = 'input'
-> extends BsPrefixComponent<As, FormControlProps> {
+> extends BsPrefixComponent<As, FormControlProps<FormControlElement>> {
   static Feedback: typeof Feedback;
 }
 
